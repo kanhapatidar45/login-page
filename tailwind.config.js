f6ad59a3caeec678ab919b2d0505edfe6df6e079
kana <patidarkanha44@email.com>
2024-06-27 19:47:43 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // Update the path as per your index.html file location
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
